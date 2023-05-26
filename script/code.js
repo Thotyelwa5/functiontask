8ac450c331f54cb1a11992c85e10f7543df76e95
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-05-26 11:35:51 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,14 @@
+
+function greet(name) {
+    console.log("Hello "+ name);
+}
+greet("Thotyelwa")
+function multiply(num1,num2) {
+    console.log(num1*num2);
+}
+multiply(7,3)
+function tableOfFive(num) {
+   return num*5
+}
+let multiplyFive = tableOfFive(10)
+console.log(multiplyFive);
